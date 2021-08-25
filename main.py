ce5848e9ce4a7272fd1bd8fca8abe46f73b427bb
Mukul Periwal <65071299+Mukul-code-rep@users.noreply.github.com>
2021-08-25 00:41:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
